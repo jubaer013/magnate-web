942e7fa02c89358de87c96835edc4780c7d3a231
jubaer013 <jubaerri17@gmail.com>
2025-12-02 15:58:59 +0600

    fixed merge conflicts manually

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // script.js
 document.getElementById('menuBtn').addEventListener('click', () => {
   document.getElementById('mobileDrawer').classList.add('open');
@@ -13,20 +13,4 @@ document.getElementById('mobileDrawer').addEventListener('click', (e) => {
   if (e.target === document.getElementById('mobileDrawer')) {
     document.getElementById('mobileDrawer').classList.remove('open');
   }
-=======
-// script.js
-document.getElementById('menuBtn').addEventListener('click', () => {
-  document.getElementById('mobileDrawer').classList.add('open');
-});
-
-document.getElementById('closeBtn').addEventListener('click', () => {
-  document.getElementById('mobileDrawer').classList.remove('open');
-});
-
-// Close when clicking outside (optional)
-document.getElementById('mobileDrawer').addEventListener('click', (e) => {
-  if (e.target === document.getElementById('mobileDrawer')) {
-    document.getElementById('mobileDrawer').classList.remove('open');
-  }
->>>>>>> d13672b21f3372fbb4fa3cf536a2c0b9f822b4d1
 });
